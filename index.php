00eed98ff4424adf7574d8fc7ed55ddb9b070c52
enrju <enrju@enrju.pl>
2022-11-23 11:03:39 +0100

    added autoloader

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 declare(strict_types=1);
 
+// //switch off error raporting in production version
+// error_reporting(0);
+// ini_set('display_errors', '0');
+
+spl_autoload_register(function (string $classNamespace) {
+    $path = str_replace(['\\', 'App/'], ['/'], $classNamespace);
+    $path = 'src/' . $path . '.php';
+
+    require_once($path);
+});
+
 require_once("src/Utils/debug.php");
 
 dump("Hello PHP");
