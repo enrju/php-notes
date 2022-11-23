00d31aaf4c5211046647f7b197ed0dd49c0be647
enrju <enrju@enrju.pl>
2022-11-23 18:39:29 +0100

    added method validateConfig() and using ConfigurationException in AbstractModel

--- src/Model/AbstractModel.php
+++ src/Model/AbstractModel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use App\Exception\ConfigurationException;
 use App\Exception\StorageException;
 use PDO;
 use PDOException;
@@ -15,15 +16,28 @@ abstract class AbstractModel
     public function __construct(array $config)
     {
         try {
+            $this->validateConfig($config);
             $this->createConnection($config);
         } catch (PDOException $e) {
-            throw new StorageException('Błąd połaczenia z DB');
+            throw new StorageException('Błąd połączenia z DB');
+        }
+    }
+
+    private function validateConfig(array $config): void
+    {
+        if (
+            empty($config['host'])
+            || empty($config['database'])
+            || empty($config['user'])
+            // || empty($config['password'])   //empty string for localhost
+        ) {
+            throw new ConfigurationException('Błąd konfiguracja DB');
         }
     }
 
     private function createConnection(array $config): void
     {
-        $dns = $dns = "mysql:host={$config['host']};dbname={$config['database']}";
+        $dns = "mysql:host={$config['host']};dbname={$config['database']}";
 
         $this->conn = new PDO(
             $dns,
